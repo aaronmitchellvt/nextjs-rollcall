781a8a839e0ee88a85a3a36fcbc8169dd208f652
aaron mitchell <aaronmitchell9902@gmail.com>
2023-02-03 21:51:38 -0500

    supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,9 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-console.log("url: ", url);
-console.log("key: ", key);
-
 const supabase = createClient(url, key);
 
 export default supabase;
